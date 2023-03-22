055d82715a5d16a0908efeba46dc317d06656052
sverma58591 <sverma@gammastack.com>
2023-03-22 17:34:16 +0530

    Segregation of role Done

--- EMentors/app/controllers/application_controller.rb
+++ EMentors/app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     protected
     def after_sign_in_path_for(resource)
-        home_index_path
+        if current_user.role == 'teacher'
+            teachers_index_path
+        else 
+            students_index_path
+        end
     end
     def after_sign_out_path_for(resource)
         new_user_session_path
     end
+    # def after_sign_up_path_for(resource)
+    #     new_user_session_path
+    # end
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
