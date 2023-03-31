d4889cd1c9ac001d4bf1c87ffc758dfb51338013
sverma58591 <sverma@gammastack.com>
2023-03-31 13:11:12 +0530

    Updated Subscriptions

--- EMentors/app/controllers/student/subscriptions_controller.rb
+++ EMentors/app/controllers/student/subscriptions_controller.rb
@@ -3,5 +3,9 @@ module Student
         def index
             @courses = Course.where(id: current_user.purchases.pluck(:course_id))
         end
+
+        def show 
+            @course = Course.find(params[:id])
+        end
     end
 end
