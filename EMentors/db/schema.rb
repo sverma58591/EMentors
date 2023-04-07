557567aa4c64a70d577253566f3fa0242c04dccc
sverma58591 <sverma@gammastack.com>
2023-04-07 12:08:39 +0530

    Rearranged Files

--- EMentors/db/schema.rb
+++ EMentors/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_114633) do
+ActiveRecord::Schema.define(version: 2023_04_07_055251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2023_04_03_114633) do
   end
 
   create_table "payments", force: :cascade do |t|
-    t.date "payment_date"
     t.bigint "user_id", null: false
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
